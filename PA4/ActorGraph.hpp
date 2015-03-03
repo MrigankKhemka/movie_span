23901f915bea940e5c26174f0dc9d892dc3ac72b
hff1014 <fhuo@ucsd.edu>
2015-03-02 19:09:54 -0800

    add header

--- PA4/ActorGraph.hpp
+++ PA4/ActorGraph.hpp
@@ -1,12 +1,20 @@
 /*
- * ActorGraph.hpp
- * Author: Adrian Guthals
- * Date: 2/24/2015
- *
- * This file is meant to exist as a container for starter code that you can use to read the input file format
- * defined in movie_casts.tsv. Feel free to modify any/all aspects as you wish.
- */
-
+*Name:Xinyu Qian, Fangfei Huo
+*date:2015.3.1
+*cse 100 assignment 4
+*a class for ActorGraph,which actor are the nodes of the graph, 
+*the movies which are casted by actor pairs are the edges between these two actors 
+* Description of its member:
+* priority_queue movie: all movies which the actor took part in, which are 
+*                       placed in a priority queue in which the lower weight has higher priority
+* unordered_map actor_map: a hash table contains all names of actors appeared in the input file and the pointers to those ActorNodes, 
+*                         the key for hash table is actor name, and the content is ActorNode*
+* unordered_map movie_map: a hash table contains all names of movies appeared in the input file and the pointers to those Movie*, 
+*                         the key for hash table is actor name, and the content is ActorNode*
+* 
+* initialize: set the name as the input actor name 
+*             set dist=60000, done=false, pre=NULL to meet Dijkstra's algorithm's requirement
+*/
 #ifndef ACTORGRAPH_HPP
 #define ACTORGRAPH_HPP
 #include <string>
--- PA4/Movie.hpp
+++ PA4/Movie.hpp
@@ -2,6 +2,17 @@
 *Name:Xinyu Qian, Fangfei Huo
 *date:2015.3.1
 *cse 100 assignment 4
+*a class for Movie and edge 
+*since Movie and edge have a one-to-one correspondence so use a single class to describe them both
+* Description of its member:
+* vector cast: all actors's name who casted in a movie
+* weight: each movie has a weight computed by it's released year 
+* year:the year of movie released             
+* index: a string used to distinguish a movie from other movies, it is composed of movie title and movie year 
+* title:the name of the movie
+* 
+* initialize: set the name as the input actor name 
+*             set the index,year,weight,title as the input movie's index,year,weight and title
 */
 #ifndef MOVIE_HPP
 #define MOVIE_HPP
@@ -21,6 +32,11 @@ using namespace std;
                           string title;
 
 			  Movie(string index, int year, int weight,string title) : index(index), weight(weight), year(year),title(title){ }
+
+            /*the compare operator used in the priority queue of movie
+             * the lower weight movie has higher priority
+             */
+
 
 			  bool comp(Movie* one, Movie* other){
 				  return (one->weight) < (other->weight);
